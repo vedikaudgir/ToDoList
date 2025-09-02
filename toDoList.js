b2ea4ecee78de98d28a1819b19bf9ca27256c805
Vedika Udgir <vedikaudgir@gmail.com>
2025-09-03 00:18:09 +0530

    added the local storage features

--- toDoList.js
+++ toDoList.js
@@ -4,46 +4,68 @@ const submitButton = document.getElementById("submit-button");
 const listArea = document.getElementById("list-area");
 
 formArea.addEventListener("submit", function (e) {
-  e
-    .preventDefault /*avoids the webpage from resetting*/
-    ();
+    e.preventDefault();
 
-  const task = mainInput.value
-    .trim /*trim removes the extra spaces from back and front*/
-    ();
-  if (task == "") {
-    return;
-  }
-
-  const newTask = document.createElement("li");
-  newTask.textContent = task;
-
-  const doneButton = document.createElement("button");
-  doneButton.innerText = "Completed ✅";
-  doneButton.classList.add("done-button");
-  doneButton.addEventListener("click", function (e) {
-    if (e.target.matches("button")) {
-      e.target.parentNode.parentNode.remove();
+    const task = mainInput.value.trim();
+    if (task == "") 
+    {
+        return;
     }
-  });
-
-  const removeButton = document.createElement("button");
-  removeButton.innerText = "Remove ❌";
-  removeButton.classList.add("remove-button");
-  removeButton.addEventListener("click", function (e) {
-    if (e.target.matches("button")) {
-      e.target.parentNode.parentNode.remove();
-    }
-  });
-
-  const buttonContainer = document.createElement("div");
-  buttonContainer.classList.add("button-container");
-
-  buttonContainer.appendChild(doneButton);
-  buttonContainer.appendChild(removeButton);
-
-  newTask.appendChild(buttonContainer);
-  listArea.appendChild(newTask);
-
-  listArea.appendChild(newTask);
+    addTasks(task);
+    saveTasks();
+    mainInput.value = "";
 });
+
+function addTasks(task) {
+    const newTask = document.createElement("li");
+    newTask.textContent = task;
+    
+    const doneButton = document.createElement("button");
+    doneButton.innerText = "Completed ✅";
+    doneButton.classList.add("done-button");
+    doneButton.addEventListener("click", function (e) {
+        if (e.target.matches("button")) {
+            e.target.parentNode.parentNode.remove();
+        }
+        saveTasks();
+    });
+    
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove ❌";
+    removeButton.classList.add("remove-button");
+    removeButton.addEventListener("click", function (e) {
+        if (e.target.matches("button")) {
+            e.target.parentNode.parentNode.remove();
+        }
+        saveTasks();
+    });
+    
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("button-container");
+    
+    buttonContainer.appendChild(doneButton);
+    buttonContainer.appendChild(removeButton);
+    
+    newTask.appendChild(buttonContainer);
+    listArea.appendChild(newTask);
+    
+}
+
+function saveTasks() {
+  let tasks = [];
+  document.querySelectorAll("#list-area li").forEach((li) => {
+        tasks.push(li.firstChild.textContent.trim());
+      }
+    );
+
+  localStorage.setItem("tasks",JSON.stringify(tasks));
+}
+
+function loadTasks() {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach((task) => {
+    addTasks(task);
+  });
+}
+
+window.addEventListener("load", loadTasks);
