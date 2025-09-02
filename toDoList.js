367cf6c33a2c986d491e07c51aece511c0b2d6e5
Vedika Udgir <vedikaudgir@gmail.com>
2025-09-02 21:29:18 +0530

    appended-the-button-and-list

--- toDoList.js
+++ toDoList.js
@@ -8,21 +8,23 @@ formArea.addEventListener("submit", function(e)
     e.preventDefault/*avoids the webpage from resetting*/();
 
     const task = mainInput.value.trim/*trim removes the extra spaces from back and front*/();
-    if (task == "") 
+    if (task == "")
     {
-        return;   
+        return;
     }
 
-    const newTask = document.createElement("li")
+    const newTask = document.createElement("li");
     newTask.textContent = task;
-
+    
     const doneButton = document.createElement("button");
     doneButton.innerText = "Completed ✅";
     doneButton.addEventListener("click", function (e)
     {
-        if (e.target.matches("button")) {
-            e.target.parentNode.remove();
-        }        
-    });
-    
+        if (e.target.matches("button"))
+            {
+                e.target.parentNode.remove();
+            }
+        });
+        newTask.appendChild(doneButton);
+        listArea.appendChild(newTask);
 });
