cc55ad9265422309a94ddc456562c064ed98eed7
Vedika Udgir <vedikaudgir@gmail.com>
2025-09-02 23:28:40 +0530

    added-remmove-button

--- toDoList.js
+++ toDoList.js
@@ -3,29 +3,47 @@ const mainInput = document.getElementById("main-input");
 const submitButton = document.getElementById("submit-button");
 const listArea = document.getElementById("list-area");
 
-formArea.addEventListener("submit", function(e)
-{
-    e.preventDefault/*avoids the webpage from resetting*/();
+formArea.addEventListener("submit", function (e) {
+  e
+    .preventDefault /*avoids the webpage from resetting*/
+    ();
 
-    const task = mainInput.value.trim/*trim removes the extra spaces from back and front*/();
-    if (task == "")
-    {
-        return;
+  const task = mainInput.value
+    .trim /*trim removes the extra spaces from back and front*/
+    ();
+  if (task == "") {
+    return;
+  }
+
+  const newTask = document.createElement("li");
+  newTask.textContent = task;
+
+  const doneButton = document.createElement("button");
+  doneButton.innerText = "Completed ✅";
+  doneButton.classList.add("done-button");
+  doneButton.addEventListener("click", function (e) {
+    if (e.target.matches("button")) {
+      e.target.parentNode.parentNode.remove();
     }
+  });
 
-    const newTask = document.createElement("li");
-    newTask.textContent = task;
-    
-    const doneButton = document.createElement("button");
-    doneButton.innerText = "Completed ✅";
-    doneButton.classList.add("done-button");
-    doneButton.addEventListener("click", function (e)
-    {
-        if (e.target.matches("button"))
-            {
-                e.target.parentNode.remove();
-            }
-        });
-        newTask.appendChild(doneButton);
-        listArea.appendChild(newTask);
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "Remove ❌";
+  removeButton.classList.add("remove-button");
+  removeButton.addEventListener("click", function (e) {
+    if (e.target.matches("button")) {
+      e.target.parentNode.parentNode.remove();
+    }
+  });
+
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList.add("button-container");
+
+  buttonContainer.appendChild(doneButton);
+  buttonContainer.appendChild(removeButton);
+
+  newTask.appendChild(buttonContainer);
+  listArea.appendChild(newTask);
+
+  listArea.appendChild(newTask);
 });
